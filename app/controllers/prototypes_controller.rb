4a8fa6aca00fcd71fc6f67b4c2e9abe6f25539e0
tomoyasoda <sodatomoya1@gmail.com>
2021-05-31 18:57:16 +0900

    issue解決1

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,8 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -54,6 +56,7 @@ class PrototypesController < ApplicationController
   end
 
   def contributor_confirmation
+    @prototype = Prototype.find(params[:id])
     redirect_to root_path unless current_user == @prototype.user
   end
 
